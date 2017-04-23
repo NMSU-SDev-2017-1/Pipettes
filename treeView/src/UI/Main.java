6647cda59ea4b32439eccfa8d958477d620ad2c8
Jonathan <Jonnaa96@nmsu.edu>
2017-04-22 18:55:28 -0600

    Created error scenes, and retrieved drawing specifications to use in other scenes.

--- treeView/src/UI/Main.java
+++ treeView/src/UI/Main.java
@@ -1,18 +1,23 @@
 package UI;
 
 import java.io.IOException;
+
+import UI.view.drawController;
 import javafx.application.*;
 import javafx.event.EventHandler;
 import javafx.fxml.*;
 import javafx.scene.*;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.*;
+
 public class Main extends Application{
 	private static Stage primaryStage;
 	private static BorderPane mainLayout;
 	
+	public static boolean drawopen;
 	@Override
 	public void start(Stage primaryStage)throws Exception{
+		drawopen=false;
 		this.primaryStage = primaryStage;
 		this.primaryStage.setTitle("Project Name");
 		showMainView();
@@ -41,6 +46,11 @@ public class Main extends Application{
 		});
 		
 	}
+	//////////////////////////////////////////////////////////////////////////
+	//
+	//	Exit check stage
+	//
+	//////////////////////////////////////////////////////////////////////////
 	
 	public static void showExit() throws IOException{
 		FXMLLoader loader = new FXMLLoader();
@@ -55,6 +65,11 @@ public class Main extends Application{
 		exitStage.setScene(exitScene);
 		exitStage.showAndWait();
 	} 
+	//////////////////////////////////////////////////////////////////////////
+	//
+	//	Draw and Dispense
+	//
+	//////////////////////////////////////////////////////////////////////////
 	
 	public static void showDraw() throws IOException{
 		FXMLLoader loader = new FXMLLoader();
@@ -82,6 +97,91 @@ public class Main extends Application{
 		Scene drawScene = new Scene(dispense);
 		drawStage.setScene(drawScene);
 		drawStage.showAndWait();
+	}
+	
+	//////////////////////////////////////////////////////////////////////////
+	//
+	//	Error stages
+	//
+	//////////////////////////////////////////////////////////////////////////
+	
+	public static void showdConError() throws IOException{
+		FXMLLoader loader = new FXMLLoader();
+		loader.setLocation(Main.class.getResource("errors/dCon.fxml"));
+		BorderPane dCon = loader.load();
+		
+		Stage dConStage = new Stage();
+		dConStage.setTitle("Project Name");
+		dConStage.initModality(Modality.WINDOW_MODAL);
+	    dConStage.initOwner(primaryStage);
+		Scene dConScene = new Scene(dCon);
+		dConStage.setScene(dConScene);
+		dConStage.showAndWait();
+	}
+	public static void showdUnitError() throws IOException{
+		FXMLLoader loader = new FXMLLoader();
+		loader.setLocation(Main.class.getResource("errors/dUnit.fxml"));
+		BorderPane dUnit = loader.load();
+		
+		Stage dUnitStage = new Stage();
+		dUnitStage.setTitle("Project Name");
+		dUnitStage.initModality(Modality.WINDOW_MODAL);
+	    dUnitStage.initOwner(primaryStage);
+		Scene dUnitScene = new Scene(dUnit);
+		dUnitStage.setScene(dUnitScene);
+		dUnitStage.showAndWait();
+	}
+	public static void showdisConError() throws IOException{
+		FXMLLoader loader = new FXMLLoader();
+		loader.setLocation(Main.class.getResource("errors/disCon.fxml"));
+		BorderPane disCon = loader.load();
+		
+		Stage disConStage = new Stage();
+		disConStage.setTitle("Project Name");
+		disConStage.initModality(Modality.WINDOW_MODAL);
+	    disConStage.initOwner(primaryStage);
+		Scene disConScene = new Scene(disCon);
+		disConStage.setScene(disConScene);
+		disConStage.showAndWait();
+	}
+	public static void showdisUnitError() throws IOException{
+		FXMLLoader loader = new FXMLLoader();
+		loader.setLocation(Main.class.getResource("errors/disUnit.fxml"));
+		BorderPane disUnit = loader.load();
+		
+		Stage disUnitStage = new Stage();
+		disUnitStage.setTitle("Project Name");
+		disUnitStage.initModality(Modality.WINDOW_MODAL);
+	    disUnitStage.initOwner(primaryStage);
+		Scene dUnitScene = new Scene(disUnit);
+		disUnitStage.setScene(dUnitScene);
+		disUnitStage.showAndWait();
+	}
+	public static void showTextError() throws IOException{
+		FXMLLoader loader = new FXMLLoader();
+		loader.setLocation(Main.class.getResource("errors/text.fxml"));
+		BorderPane text = loader.load();
+		
+		Stage textStage = new Stage();
+		textStage.setTitle("Project Name");
+		textStage.initModality(Modality.WINDOW_MODAL);
+		textStage.initOwner(primaryStage);
+		Scene textScene = new Scene(text);
+		textStage.setScene(textScene);
+		textStage.showAndWait();
+	}
+	public static void showDisError() throws IOException{
+		FXMLLoader loader = new FXMLLoader();
+		loader.setLocation(Main.class.getResource("view/disError.fxml"));
+		BorderPane disError = loader.load();
+		
+		Stage disErrorStage = new Stage();
+		disErrorStage.setTitle("Project Name");
+		disErrorStage.initModality(Modality.WINDOW_MODAL);
+		disErrorStage.initOwner(primaryStage);
+		Scene disErrorScene = new Scene(disError);
+		disErrorStage.setScene(disErrorScene);
+		disErrorStage.showAndWait();
 	}
 	
 	public static void main(String[] args){
--- treeView/src/UI/view/MainController.java
+++ treeView/src/UI/view/MainController.java
@@ -1,30 +1,49 @@
 package UI.view;
 
 import java.io.IOException;
+import java.util.Arrays;
+
 import UI.Main;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.collections.ObservableSet;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
+import javafx.scene.control.cell.TextFieldListCell;
 import javafx.stage.Window;
 
 public class MainController {
-
+	
+	//ObservableSet<String> items = FXCollections.observableSet();
+	ObservableList<String> items = FXCollections.observableArrayList("Hello", "Bye");
 	@FXML
-	private TreeView<String> treeView;
+	public TreeView<String> treeView;
 	@FXML 
-	private ListView listView;
+	public ListView<String> listView;
+	//public static ListView newList = listView;
 	@FXML
 	private Button okButton;
 	@FXML
 	private Button canButton;
 	
-	
+	//public static String[] procedure;
+	public void list(){
+		//System.out.println("You made it");
+		//listView.getItems().add("byee");
+	}
 	@FXML
 	private void drawButton() throws IOException{
 		Main.showDraw();
 	}
 	@FXML
 	private void dispenseButton() throws IOException{
-		Main.showDispense();
+		drawController dC = new drawController();
+		if((dC.dopen)==false){
+			Main.showDisError();
+		}
+		else{
+			Main.showDispense();
+		}
 	}
 	@FXML
 	private void okButton(){
@@ -52,18 +71,10 @@ public class MainController {
 		
 		child1.getChildren().addAll(child1_1,child1_2,child1_3);
 		
-		TreeItem<String> child2_1 = new TreeItem<>("Child2.1");
-		TreeItem<String> child2_2 = new TreeItem<>("Child2.2");
-		TreeItem<String> child2_3 = new TreeItem<>("Child3.3");
-		
-		child2.getChildren().addAll(child2_1,child2_2,child2_3);
-		
-		TreeItem<String> child3_1 = new TreeItem<>("Child3.1");
-		TreeItem<String> child3_2 = new TreeItem<>("Child3.2");
-		TreeItem<String> child3_3 = new TreeItem<>("Child3.3");
-		
-		child3.getChildren().addAll(child3_1,child3_2,child3_3);
-		
+		listView.setEditable(true);
+		listView.setCellFactory(TextFieldListCell.forListView());
+		listView.setItems(items);
 		treeView.setRoot(root);
+		
 	}
 }
